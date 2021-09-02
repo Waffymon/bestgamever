2a2e00f2bfbba814173bed6b26f56a4267b3c68f
Waffymon <84250280+Waffymon@users.noreply.github.com>
2021-09-02 17:51:07 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -16,8 +16,16 @@ var cuadrod5
 var cuadrod6
 var cuadrod7
 
-//morado a amarillo
- var cuadodro1
+var cuadod11
+var cuadrod22
+var cuadrod33
+var cuadrod44
+var cuadrod55
+var cuadrod66
+var cuadrod77
+
+
+
 
 
 
@@ -37,16 +45,35 @@ function setup(){
    cuadrito3.shapeColor="yellow";
    cuadrito4.shapeColor="purple";
 
+//naranja a morado
+ cuadod1=createSprite(75,180,150,90);
+ cuadrod2=createSprite(75,285,150,90);
+ cuadrod3=createSprite(75,390,150,90);
+ cuadrod4=createSprite(75,495,150,90);
+ cuadrod5=createSprite(75,600,150,90);
+ cuadrod6=createSprite(75,705,150,90);
+ cuadrod7=createSprite(75,805,150,90);
 
-cuadod1=createSprite(75,180,150,90);
-cuadrod2=createSprite(75,290,150,90);
-cuadrod3=createSprite(75,400,150,90);
-cuadrod4=createSprite(75,510,150,90);
-cuadrod5=createSprite(75,620,150,90);
-cuadrod6=createSprite(75,730,150,90);
-cuadrod7=createSprite(75,840,150,90);
+ //azul a amarillo
 
-cuadodro1=createSprite(200,925,90,150)
+ cuadod11=createSprite(925,180,150,90);
+ cuadrod22=createSprite(925,285,150,90);
+ cuadrod33=createSprite(925,390,150,90);
+ cuadrod44=createSprite(925,495,150,90);
+ cuadrod55=createSprite(925,600,150,90);
+ cuadrod66=createSprite(925,705,150,90);
+ cuadrod77=createSprite(925,805,150,90);
+
+ //morado a amarillo
+ cuadod111=createSprite(180,925,90,150);
+ cuadrod222=createSprite(925,285,150,90);
+ cuadrod333=createSprite(925,390,150,90);
+ cuadrod444=createSprite(925,495,150,90);
+ cuadrod555=createSprite(925,600,150,90);
+ cuadrod666=createSprite(925,705,150,90);
+ cuadrod777=createSprite(925,805,150,90);
+
+
 
 
 }
